fc36a4e0a683e45b72e6734a8a1ff677374789f6
Murilo <murilo.polla@gmail.com>
2025-01-29 15:58:13 +0100

    setting up first pipeline

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pre_processing.load_data import load_data
+from pre_processing.encoding import encode_data
+from modeling.train import create_model
+from modeling.predict import predict
+from data_handler.data_handler import write_CSV
+
+#main pipeline
+def run_pipeline():
+
+  #1. Load CSV and Format
+  df = load_data()
+  assert isinstance(df, pd.DataFrame)  
+
+  #2. Encoding
+  df = encode_data(df)
+  assert isinstance(df, pd.DataFrame)  
+
+  #3. Training
+  model = create_model(df)
+
+  #4. Predict
+  pred = predict(model, df)
+
+  #5. Write CSV
+  outputh_path = ""
+  write_csv(pred, outputh_path)
+
+  print("Script finished")
+
+if __name__ == "__main__":
+  run_pipeline()  
---
+++ src/pre_processing/encoding.py
@@ -0,0 +1,4 @@
+def encode_data(df):
+    ##Do encoding code here
+    encoded_df = df
+    return encoded_df
