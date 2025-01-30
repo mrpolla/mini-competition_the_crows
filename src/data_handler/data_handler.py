534573da6f75a74b34beacd2b601dcade398a3ab
KhashiYaar <khashayar.razghandi@gmail.com>
2025-01-31 00:10:44 +0100

    small polishings (Paul)

--- src/data_handler/data_handler.py
+++ src/data_handler/data_handler.py
@@ -1,30 +1,34 @@
+"""Data Handling Funcions."""
+
 import pandas as pd
+
 
 def load_data():
     try:
-        df_train_X = pd.read_csv('../data/raw/train_values.csv')
-        df_train_y = pd.read_csv('../data/raw/train_labels.csv')
-        df_test_X = pd.read_csv('../data/raw/test_values.csv')
+        df_train_X = pd.read_csv("../data/raw/train_values.csv")
+        df_train_y = pd.read_csv("../data/raw/train_labels.csv")
+        df_test_X = pd.read_csv("../data/raw/test_values.csv")
     except FileNotFoundError:
         print("Error: File not found. Please check the file path.")
 
     nrows_train = df_train_X.shape[0]
     nrows_test = df_test_X.shape[0]
-    nrows_tot = nrows_train+nrows_test
+    nrows_tot = nrows_train + nrows_test
 
-    df_train_X['train'] = 1
-    df_test_X['train'] = 0
+    df_train_X["train"] = 1
+    df_test_X["train"] = 0
     df_X = pd.concat([df_train_X, df_test_X], ignore_index=True)
-    df = pd.merge(df_X, df_train_y, on='building_id', how='left')
+    df = pd.merge(df_X, df_train_y, on="building_id", how="left")
 
-    assert df.shape[0]==nrows_tot, "Incorrect number of rows"
+    assert df.shape[0] == nrows_tot, "Incorrect number of rows"
     assert isinstance(df, pd.DataFrame), "No pandas dataframe returned"
 
     return df
 
+
 def write_data(df):
     try:
-        df.to_csv('../data/processed/submission.csv', index=False)
+        df.to_csv("../data/processed/submission.csv", index=False)
         print(f"File written")
     except:
         print("Error: Could not write file.")
