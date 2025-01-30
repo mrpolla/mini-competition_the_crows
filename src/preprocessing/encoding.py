4383d525fe1d0b3117d18329a4e70a00e8aeb455
KhashiYaar <khashayar.razghandi@gmail.com>
2025-01-30 15:26:25 +0100

    2nd try cat.ft encoding onehot

--- src/preprocessing/encoding.py
+++ src/preprocessing/encoding.py
@@ -1,4 +1,18 @@
 
+# Convert suspicious numerical columns to categorical (by converting integers to strings)
+# here values are assigned inside definging the fucniton 
+
+def convert_numerical_to_categorical(df):
+    numerical_columns_to_convert = ['geo_level_1_id', 'geo_level_2_id', 'geo_level_3_id']
+    for col in numerical_columns_to_convert:
+        df[col] = df[col].astype(str)  # Convert to string (categorical)
+    return df
+
+df = convert_numerical_to_categorical(df)
+
+
+
+
 import pandas as pd
 
 # # 2nd try encoding categorical_columns: Onehot encoding 
