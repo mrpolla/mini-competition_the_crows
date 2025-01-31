f60274a5dce372ab0a5c8502a3c5f85a7eedf773
Murilo <murilo.polla@gmail.com>
2025-01-31 10:06:27 +0100

    integrated grid search and lightgbm

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -35,7 +35,7 @@ def create_model_lightgbm(df):
   y_pred = best_model.predict(X_test)
   accuracy = accuracy_score(y_test, y_pred)
   print("Best Model Accuracy:", accuracy)
-  best_model.save_model('lgbm_model.txt')
+  best_model.booster_.save_model('lgbm_model.txt')
 ##
 
   return best_model
@@ -76,23 +76,17 @@ def create_model_with_CV(df):
 
   # Set XGBoost parameters for hypertuning
   param_grid = {
-    'n_estimators': [100, 200, 300],         # Number of boosting rounds
-    'learning_rate': [0.01, 0.05, 0.1, 0.2], # Step size shrinkage
-    'max_depth': [3, 5, 7],                  # Maximum depth of trees
-    'min_child_weight': [1, 3, 5],           # Minimum sum of instance weight in a child
-    'subsample': [0.7, 0.8, 1.0],            # Fraction of samples used per tree
-    'colsample_bytree': [0.7, 0.8, 1.0],     # Fraction of features used per tree
-    'gamma': [0, 0.1, 0.2, 0.3],             # Minimum loss reduction to split
-    'reg_alpha': [0, 0.01, 0.1],             # L1 regularization
-    'reg_lambda': [0.1, 1, 10],              # L2 regularization
+    'n_estimators': [50, 100, 150],  # Lower and higher values for boosting rounds
+    'learning_rate': [0.01, 0.05, 0.1],  # A good range of learning rates
+    'max_depth': [3, 5]  # Shallow to deeper trees
   }
 
   # Define XGBoost model for multi-class classification
-  xgb = xgb.XGBClassifier(objective='multi:softmax', num_class=len(set(y_train)))
+  xgb_model = xgb.XGBClassifier(objective='multi:softmax', num_class=len(set(y_train)))
   
   # Perform Grid Search
-  grid_search = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=3, scoring='accuracy', verbose=1, n_jobs=-1)
-  grid_search.fit(X_train, y_train)
+  grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=10, n_jobs=-1)
+  grid_search.fit(X_train, y_train) 
   
   # Best hyperparameters
   print("Best Parameters:", grid_search.best_params_)
